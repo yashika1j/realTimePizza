2f2055afffb0cd41fa066be48c1c7d1faf8b4766
Yashika Bansal <yashikabansal1j@gmail.com>
2023-05-03 23:48:02 +0530

    add cart page, auth

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 const express = require('express')
+const expressEjsLayouts = require('express-ejs-layouts')
 
 const app = express()
 
 
 const path = require('path')
 const PORT = process.env.PORT  || 5500
-//assets
+
+
+
+// set template engine
+app.use(expressEjsLayouts)
+app.set('views' , path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+
 app.use(express.static('public'))
 app.get('/', (req , res) =>{
     res.render('home')
 })
-// set template engine
-app.set('views' , path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+app.get('/cart', (req , res) => {
+    res.render('customers/cart')
+
+ })
+
+ app.get('/login', (req , res) => {
+    res.render('auth/login')
+
+ })
+
+ app.get('/register', (req , res) => {
+    res.render('auth/register')
+
+ })
+
+
 app.listen(PORT, () =>
 {
     
